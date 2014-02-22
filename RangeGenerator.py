1bcb127308534f9be102a8e45521c89fbc60fbc7
Weerasak Chongnguluam <singpor@gmail.com>
2014-02-22 17:06:07 +0700

    No if use logical assignment

--- RangeGenerator.py
+++ RangeGenerator.py
@@ -7,19 +7,26 @@ class RangeGenerator:
         begin_number = int(number_list[0])
         end_number = int(number_list[1])
 
+        notation_type = begin_notation+end_notation
+
+        result_set =(   
+                        (notation_type == "()" and self.generate_begin_end(begin_number+1, end_number - 1)) or 
+                        (notation_type == "(]" and self.generate_begin_end(begin_number+1, end_number)) or 
+                        (notation_type == "[)" and self.generate_begin_end(begin_number, end_number - 1)) or 
+                        (notation_type == "[]" and self.generate_begin_end(begin_number, end_number))
+                    )
         
-        generator_function = self.generator_factory(begin_notation, end_notation)
-        return generator_function(begin_number, end_number)
+        return result_set
         
     def generate_begin_end(self, begin_number, end_number):
         return "{" + str(range(begin_number, end_number+1)).replace(' ',"")[1:-1] + "}"
 
-    def generator_factory(self, begin_notation, end_notation):
-        GENERATOR_DICTIONARY = {
-            "()" : lambda begin_number, end_number : self.generate_begin_end(begin_number+1, end_number - 1),
-            "(]" : lambda begin_number, end_number : self.generate_begin_end(begin_number+1, end_number),
-            "[)" : lambda begin_number, end_number : self.generate_begin_end(begin_number, end_number - 1),
-            "[]" : lambda begin_number, end_number : self.generate_begin_end(begin_number, end_number),
-        }
+    # def generator_factory(self, begin_notation, end_notation):
+    #     GENERATOR_DICTIONARY = {
+    #         "()" : lambda begin_number, end_number : self.generate_begin_end(begin_number+1, end_number - 1),
+    #         "(]" : lambda begin_number, end_number : self.generate_begin_end(begin_number+1, end_number),
+    #         "[)" : lambda begin_number, end_number : self.generate_begin_end(begin_number, end_number - 1),
+    #         "[]" : lambda begin_number, end_number : self.generate_begin_end(begin_number, end_number),
+    #     }
 
-        return GENERATOR_DICTIONARY[begin_notation+end_notation]
+    #     return GENERATOR_DICTIONARY[begin_notation+end_notation]
